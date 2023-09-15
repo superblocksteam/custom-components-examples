8880cbda35db364a7babeec865fc338ac614ffe9
Jesse Shatsky <jesseshatsky@gmail.com>
2023-09-15 12:42:20 -0400

    added document scanner component

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # custom-components-examples
+
 Repository of example custom components built and maintained by the Superblocks team
+
+In order to match the development pattern for custom components, each example component or set of components is uploaded as part of an app directory.
+
+You can easily download any example component from this repository and copy it into your app directory. Be sure to also copy over the `package.json` & `package-lock.json` files to obtain the relevant dependencies.
---
+++ document_scanner_webinar/components/DocumentScanner/component.tsx
@@ -0,0 +1,159 @@
+import React, { useRef, useState, useCallback, useEffect } from "react";
+import Webcam from "react-webcam";
+import {
+  useSuperblocksContext,
+  useSuperblocksWidgetSize,
+} from "@superblocksteam/custom-components";
+import { type Props, type EventTriggers } from "./types";
+import { Button, Select, Empty } from "antd";
+import {
+  VideoCameraAddOutlined,
+  StopOutlined,
+  CameraOutlined,
+} from "@ant-design/icons";
+import Confetti from "react-confetti";
+import ReactDOM from "react-dom";
+const { Option } = Select;
+
+const VIDEO_HEIGHT_OFFSET = 60;
+
+export default function Component({ rainConfettiOnCapture }: Props) {
+  const [isShowVideo, setIsShowVideo] = useState<boolean>(false);
+  const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
+  const videoElement = useRef<Webcam | null>(null);
+
+  const { widthPx, heightPx } = useSuperblocksWidgetSize();
+
+  const handleDevices = useCallback(
+    (mediaDevices: MediaDeviceInfo[]) =>
+      setDevices(mediaDevices.filter(({ kind }) => kind === "videoinput")),
+    [setDevices],
+  );
+
+  // call useSuperblocksContext() to get the updateProperties method and event triggers
+  const {
+    updateProperties,
+    events: { onImageCapture },
+  } = useSuperblocksContext<Props, EventTriggers>();
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .enumerateDevices()
+      .then(handleDevices)
+      .catch((error) => console.error("Error enumerating devices: ", error));
+  }, [handleDevices]);
+
+  const videoHeight = heightPx - VIDEO_HEIGHT_OFFSET;
+
+  const videoConstraints = {
+    width: widthPx,
+    height: videoHeight,
+  };
+
+  const startCam = () => setIsShowVideo(true);
+
+  const stopCam = () => {
+    // Add more type safety here, if applicable.
+    const stream = videoElement.current?.stream;
+    stream?.getTracks().forEach((track) => track.stop());
+    setIsShowVideo(false);
+  };
+
+  // Handle showing confetti
+
+  const [showConfetti, setShowConfetti] = useState(false);
+
+  useEffect(() => {
+    let confettiRoot: HTMLDivElement;
+    if (showConfetti && rainConfettiOnCapture) {
+      confettiRoot = document.createElement("div");
+      confettiRoot.style.position = "fixed";
+      confettiRoot.style.top = "0";
+      confettiRoot.style.left = "0";
+      confettiRoot.style.width = "100vw";
+      confettiRoot.style.height = "100vh";
+      confettiRoot.style.zIndex = "9999";
+      confettiRoot.style.pointerEvents = "none";
+      document.body.appendChild(confettiRoot);
+    }
+
+    return () => {
+      if (confettiRoot) {
+        document.body.removeChild(confettiRoot);
+      }
+    };
+  }, [rainConfettiOnCapture, showConfetti]);
+
+  const handleConfettiShow = () => {
+    if (rainConfettiOnCapture) {
+      setShowConfetti(true);
+      setTimeout(() => setShowConfetti(false), 5500); // Hide confetti after 5.5 seconds
+    }
+  };
+
+  const handleCapture = () => {
+    const imageSrc = videoElement.current?.getScreenshot({
+      width: 1920,
+      height: 1080,
+    });
+    if (imageSrc) {
+      updateProperties({ image: imageSrc });
+      onImageCapture();
+    }
+    handleConfettiShow();
+  };
+
+  return (
+    <div>
+      {showConfetti &&
+        rainConfettiOnCapture &&
+        ReactDOM.createPortal(
+          <Confetti
+            initialVelocityX={50}
+            width={window.innerWidth}
+            height={window.innerHeight}
+          />,
+          document.body,
+        )}
+      <div style={{ flex: 1, minHeight: videoHeight }}>
+        <div className="camView">
+          {isShowVideo && (
+            <Webcam
+              screenshotFormat="image/jpeg"
+              audio={false}
+              ref={videoElement}
+              videoConstraints={videoConstraints}
+            />
+          )}
+          {!isShowVideo && (
+            <Empty imageStyle={videoConstraints} description={false} />
+          )}
+        </div>
+      </div>
+      <div style={{ flexShrink: 0 }}>
+        {isShowVideo && (
+          <>
+            <Button onClick={stopCam} icon={<StopOutlined />}>
+              Stop Video
+            </Button>
+            <Button onClick={handleCapture} icon={<VideoCameraAddOutlined />}>
+              Take Screenshot
+            </Button>
+          </>
+        )}
+        {!isShowVideo && (
+          <Button onClick={startCam} icon={<CameraOutlined />}>
+            Start Video
+          </Button>
+        )}
+        {devices.length > 1 && (
+          <Select id="deviceList" defaultActiveFirstOption={true}>
+            {devices.map((device, key) => (
+              <Option key={device.deviceId}>{device.label}</Option>
+            ))}
+          </Select>
+        )}
+      </div>
+    </div>
+  );
+}
